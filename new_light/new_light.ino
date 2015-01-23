d541db32c37fe4bae6b41e62c10e906abdf92fa4
Jim Lee <github@leftcoast.biz>
2015-01-22 23:24:53 -0800

    Got the first channel of the joystick working
    Did the groundwork for reading the joystick. this included adding the
    LC neoPixel object to the libraries from new light.

--- new_light/new_light.ino
+++ new_light/new_light.ino
@@ -11,48 +11,10 @@
 #include <servo.h>
 #include <timeObj.h>
 #include <colorObj.h>
+#include <neoPixel.h>
 
 #define NUM_LEDS 12
-
-class neoPixel : public Adafruit_NeoPixel {
-   
-      public:
-
-  // Constructor: number of LEDs, pin number, LED type.
-  // See the ADAfruit_NeoPixel.h for info on these things.
-  neoPixel(uint16_t n, uint8_t p=6, uint8_t t=NEO_GRB + NEO_KHZ800);
-  ~neoPixel();
-  
-  void setPixelColor(uint16_t n,colorObj* inColor);
-  colorObj getPixelColor(uint16_t n);
-};
-
-
- neoPixel::neoPixel(uint16_t n, uint8_t p, uint8_t t)
-   :  Adafruit_NeoPixel(n,p,t) {  }
-   
-   
- neoPixel::~neoPixel() {  }
-
-
-void neoPixel::setPixelColor(uint16_t n,colorObj* inColor) { 
-    
-    Adafruit_NeoPixel::setPixelColor(n,inColor->getRed(),inColor->getGreen(),inColor->getBlue());
- }
-  
-  
-colorObj neoPixel::getPixelColor(uint16_t n) {
-   
-   uint32_t packColor;
-   uint8_t*    colorPtr;
-   
-   packColor = Adafruit_NeoPixel::getPixelColor(n);
-   colorPtr = (uint8_t*) &packColor;
-   colorObj result(colorPtr[2],colorPtr[1],colorPtr[0]);
-   return result;
-}
-
-
+ 
 neoPixel ring(NUM_LEDS,3);
 
 //colorObj black(BLACK);
