3bb5c2af2981dccea2a0a00530d38f22f4677acf
Jim Lee <gitHub@leftcoast.biz>
2015-01-24 18:18:22 -0800

    Got the first two channels talking to the quad
    Grinding through code & wiring.

--- CnC/CnC.ino
+++ CnC/CnC.ino
@@ -13,13 +13,14 @@
 #include <servo.h>
 #include <timeObj.h>
 
-#define MIN_PULSE 1000
-#define MID_PULSE 1500
-#define MAX_PULSE 2000
+#define MIN_PULSE    1000
+#define MID_PULSE    1500
+#define MAX_PULSE    2000
+#define PULSE_OFFSET 240        // In the end, it seems like everything takes too long. By this much.
 
 #define ROLL_PIN     A0
 #define PITCH_PIN    A1
-#define YAW_PIN      A2
+#define YAW_PIN      A2 
 #define THROTTLE_PIN A3
 #define AUX_PIN      A4
 
@@ -27,10 +28,10 @@
 #define IDLE_STATE   HIGH
 #define ACTIVE_STATE LOW
 #define TIMING_PULSE 200         // microseconds.
-#define OUT_PIN      0
+#define OUT_PIN      7
 #define REFRESH_TIME 22          // miliseconds  mili NOT micro!
 
-#define NUM_CHANNELS 5
+#define NUM_CHANNELS 6          // Oddly, Dx5e has 6 channels.
 #define THROTTLE     0
 #define AILERON      1
 #define ELEVAOTOR    2
@@ -63,57 +64,95 @@ timeObj outTimer(REFRESH_TIME);
 
 void setupMappers(void) {
 
-  rollMapper.addPoint(0,MIN_PULSE);
+  rollMapper.addPoint(0,MAX_PULSE);
   rollMapper.addPoint(512,MID_PULSE);
-  rollMapper.addPoint(1023,MAX_PULSE);
+  rollMapper.addPoint(1023,MIN_PULSE);
 
   pitchMapper.addPoint(0,MIN_PULSE);
   pitchMapper.addPoint(512,MID_PULSE);
   pitchMapper.addPoint(1023,MAX_PULSE);
 
-  yawMapper.addPoint(0,MIN_PULSE);
+  yawMapper.addPoint(0,MAX_PULSE);
   yawMapper.addPoint(512,MID_PULSE);
-  yawMapper.addPoint(1023,MAX_PULSE);
+  yawMapper.addPoint(1023,MIN_PULSE);
 }
 
 
 void setup(void) {
 
-  //Serial.begin(9600);
+  Serial.begin(9600);
   setupMappers();
+  
+  // Give the array some nice non-crazy defaults.
   for (int i=0;i<NUM_CHANNELS;i++) { 
     channels[i] = MID_PULSE;
   }
+  
+  // Setup utput pin for the signal.
   pinMode(OUT_PIN, OUTPUT);     
   digitalWrite(OUT_PIN, IDLE_STATE);
 
+  // Lights, if we use 'em.
   ring.begin();
   for (int i=0;i<=NUM_LEDS;i++) {
     ring.setPixelColor(i,&black);
   }
   ring.show();
+  
+  // Fire up the timer..
   outTimer.start();
+  
+  // The plan is to use states.. Later. So for now, we're "running".
   currState = running;
 }
 
 
-void readValues(void) {
 
-  channels[THROTTLE] = throttleMapper.Map(analogRead(THROTTLE_PIN));
-  channels[AILERON] = rollMapper.Map(analogRead(ROLL_PIN));
-  channels[ELEVAOTOR] = pitchMapper.Map(analogRead(PITCH_PIN));
-  channels[RUDDER] = yawMapper.Map(analogRead(YAW_PIN));
-  channels[AUX] = auxMapper.Map(analogRead(AUX_PIN));
+// Run the light display, if there is one.
+void showControls(void) {
+  
+  colorObj green(GREEN);
+  green = green.blend(&black,20);
+  colorObj red(RED);
+  red = red.blend(&black,20);
+  mapper roll2light(MIN_PULSE,MAX_PULSE,TRANSPARENT,OPAQUE);
+  mapper pitch2light(MIN_PULSE,MAX_PULSE,TRANSPARENT,OPAQUE);
+  colorObj result = green.blend(&red,roll2light.Map(channels[AILERON]));
+  result = result.blend(&black,pitch2light.Map(channels[ELEVAOTOR]));
+  for (int i=0;i<=NUM_LEDS;i++) {
+    ring.setPixelColor(i,&result);
+  }
+  ring.show();
 }
 
 
+// Grab all the analog values from the pins, scale 'em and stuff 'em into the channel array.
+void readValues(void) {
+
+  //channels[THROTTLE] = round(throttleMapper.Map(analogRead(THROTTLE_PIN)));
+  channels[AILERON] = round(rollMapper.Map(analogRead(ROLL_PIN)));
+  channels[ELEVAOTOR] = round(pitchMapper.Map(analogRead(PITCH_PIN)));
+  //channels[RUDDER] = round(yawMapper.Map(analogRead(YAW_PIN)));
+  //channels[AUX] = round(auxMapper.Map(analogRead(AUX_PIN)));
+  Serial.print("THROTTLE  : ");Serial.println(channels[THROTTLE]);
+  Serial.print("AILERON   : ");Serial.println(channels[AILERON]);
+  Serial.print("ELEVAOTOR : ");Serial.println(channels[ELEVAOTOR]);
+  Serial.print("RUDDER    : ");Serial.println(channels[RUDDER]);
+  Serial.print("AUX       : ");Serial.println(channels[AUX]);
+  Serial.println();
+}
+
+
+// We have the array and the numnber of channels. Stuff 'em out the wire.
+// There really is nothing else going on. So, lets just use delays to make 
+// life easy for the poor code writer huh?
 void sendPulse(int* pulseList,byte numChannels) {
 
   for (int i=0;i<numChannels;i++) {
     digitalWrite(OUT_PIN, ACTIVE_STATE);     // Wake up!
     delayMicroseconds(TIMING_PULSE);               
     digitalWrite(OUT_PIN, IDLE_STATE);       // Now listen!
-    delayMicroseconds(pulseList[i]);         // time this!
+    delayMicroseconds(pulseList[i]-PULSE_OFFSET);         // time this!
   }
   digitalWrite(OUT_PIN, ACTIVE_STATE);       // end pulse.
   delayMicroseconds(TIMING_PULSE);               
@@ -121,30 +160,23 @@ void sendPulse(int* pulseList,byte numChannels) {
 }
 
 
+//This is called from the main loop if we're currently "running".
 void runLoop(void) {
 
-  if (outTimer.ding()) {
-    sendPulse(channels,NUM_CHANNELS);
-    readValues();
-    colorObj green(GREEN);
-    green = green.blend(&black,20);
-    colorObj red(RED);
-    red = red.blend(&black,20);
-    mapper roll2light(MIN_PULSE,MAX_PULSE,TRANSPARENT,OPAQUE);
-    colorObj result = green.blend(&red,roll2light.Map(channels[AILERON]));
-    for (int i=0;i<=NUM_LEDS;i++) {
-      ring.setPixelColor(i,&result);
-    }
-    ring.show();
-    outTimer.stepTime();
+  if (outTimer.ding()) {                   // Is it time yet?
+    sendPulse(channels,NUM_CHANNELS);      // Guess so. First Send the pulse out.
+    readValues();                          // Now, we know we have some time to kill. Read positioins!
+    showControls();                        // IF we have a light display. Set it now.
+    outTimer.stepTime();                   // Reset the timer from the last time it expired.
   }
 }
 
 
+// Main loop..
 void loop() {
 
-  idle();
-  switch (currState) {
+  idle();                                    // There may be background things. Let 'em run.
+  switch (currState) {                       // Handle whatever thing we're doing now.
     case running : runLoop(); break;
     //calibStick : calStickLoop(); break;
     //calibPetals : calPetalsLoop(); break;
