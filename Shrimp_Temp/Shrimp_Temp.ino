c149a0e46f4897c18222f5ef003a05d781ac6d4a
Jim Lee <gitHub@leftcoast.biz>
2015-02-02 16:11:24 -0800

    First pass at Shrimp temp up and running.

--- Shrimp_Temp/Shrimp_Temp.ino
+++ Shrimp_Temp/Shrimp_Temp.ino
@@ -27,23 +27,24 @@
 #include "LM35.h"
 
 #define NUM_SIZE   2
-#define LEFT_EDGE  90
-#define BASELINE1  70
-#define BASELINE2  163
+#define LEFT_EDGE  110
+#define BASELINE1  40
+#define BASELINE2  80
 #define PRECISION  1
 
 timeObj printTimer(1000);
-LM35 tempSensor(A4);
+LM35 tempSensor1(A4);
+LM35 tempSensor2(A5);
 
 label temp1("-000.0",NUM_SIZE);
 label temp2("-000.0",NUM_SIZE);
 
 void setup() {
   
-  Point inDest;
+  TSPoint inDest;
   
   //Serial.begin(9600);
-  if (initScreen(INV_PORTRAIT)) {
+  if (initScreen(INV_LANDSCAPE)) {
     screen->fillScreen(WHITE);
     /*
     if (initSDCard()) {
@@ -65,10 +66,21 @@ void setup() {
     temp2.setJustify(TEXT_RIGHT);
     temp2.setPrecision(PRECISION);
     
-    label label1("Temp 1:",NUM_SIZE);
+    label label1("Shrimp :       Deg F",NUM_SIZE);
+ 
     label1.setLocation(10,BASELINE1);
     label1.setJustify(TEXT_LEFT);
     label1.draw();
+    
+    label1.setValue("Sea    :       Deg F");
+    label1.setLocation(10,BASELINE2);
+    label1.draw();
+    
+    /*
+    label1.setValue("Deg. F");
+    label1.setLocation(180,BASELINE1);
+    label1.draw();
+    */
   } 
   printTimer.start();
 }
@@ -76,8 +88,11 @@ void setup() {
 void loop() {
 
   if (printTimer.ding()) {
-    temp1.setValue(tempSensor.tempF());
+    temp1.setValue(tempSensor1.tempF());
     temp1.draw();
+    
+    temp2.setValue(tempSensor2.tempF());
+    temp2.draw();
     /*
     Serial.print("Temp C  : ");
     Serial.println(tempSensor.tempC());
