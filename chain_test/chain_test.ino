fd498f7048800f72f53f2a1ca0f64996e511cc23
Jim Lee <gitHub@leftcoast.biz>
2015-02-14 16:30:21 -0800

    Yes! chainPixels works with multiple objects!
    Tested with a 12 LED ring & 8 LED light stick. Ring doing things in the
    background and the stick holding a color.

--- chain_test/chain_test.ino
+++ chain_test/chain_test.ino
@@ -20,9 +20,43 @@
 #define NUM_LEDS 12
 #define LED_PIN 3
 
+
+
+ 
+class  tialLight : public pixelGroup {
+  
+  public:
+            tialLight(void);
+            ~tialLight(void);
+            
+    virtual void   draw(void);
+    
+            boolean drawn;
+};
+
+
+tialLight::tialLight(void) : pixelGroup(8) { drawn = false; }
+
+
+tialLight::~tialLight(void) { }
+            
+            
+ void tialLight::draw(void) {
+   
+      if (!drawn) {
+        colorObj result;
+        result.setColor(&red);
+        result = result.blend(&black,75);
+        result = result.blend(&blue,15);
+        setPixels(&result);
+        drawn = true;
+      }
+ }
+
+
 chainPixels theChain(LED_PIN);
 robbyRing* ring1;
-  
+tialLight* lightStick;
   
 void setup() {
   
@@ -31,6 +65,10 @@ void setup() {
   
   ring1 = new robbyRing(NUM_LEDS);
   theChain.addGroup(ring1);
+  
+  lightStick = new tialLight();
+  theChain.addGroup(lightStick);
+  
   theChain.hookup();
 }
 
