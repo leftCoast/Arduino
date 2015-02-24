a309c0db61eb8641ea76dbf3b2c8be19fb867294
Jim Lee <gitHub@leftcoast.biz>
2015-02-24 15:04:15 -0800

    Created a folder for Rob's quad.

---
+++ Rob_s_Quad/policeBar.h
@@ -0,0 +1,34 @@
+#ifndef policeBar_h
+#define policeBar_h
+
+#include <timeObj.h>
+#include <chainPixels.h>
+
+enum pbStates { pbOff, pbFlood, pbBlink };
+
+class policeBar : public pixelGroup {
+  
+  public:
+            policeBar(void);    // Hardcoded for 8 LED light sick.
+            ~policeBar(void);
+            
+            void       lightOff(void);
+            void       floodOn(void);
+            void       blinkOn(void);
+            void       setupBlink(void);
+            colorObj*  chooseColor(int index);
+            void       setLights(int index);
+            void       blinkTime(void);
+    virtual void       draw(void);
+    
+  private:
+          boolean  init;
+          pbStates  nextState;
+          pbStates  state;
+          timeObj   flashTimer;
+          int       flashIndex;
+          colorObj  pbRed;
+          colorObj  pbBlue;
+};
+
+#endif
