59605b97d0866123da37aece55a59d5ff7366cb2
Jim Lee <github@leftcoast.biz>
2015-03-14 11:16:57 -0700

    Starting work on the color sensor

---
+++ tcs34725_testing/tcs34725_testing.ino
@@ -0,0 +1,83 @@
+#include <chainPixels.h>
+#include <liteLine.h>
+#include <neoPixel.h>
+
+#include <blinker.h>
+#include <colorObj.h>
+#include <idlers.h>
+#include <lists.h>
+#include <mapper.h>
+#include <multiMap.h>
+#include <PulseOut.h>
+#include <runningAvg.h>
+#include <servo.h>
+#include <timeObj.h>
+
+#include <Adafruit_NeoPixel.h>
+
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+/* Example code for the Adafruit TCS34725 breakout library */
+
+/* Connect SCL    to analog 5
+   Connect SDA    to analog 4
+   Connect VDD    to 3.3V DC
+   Connect GROUND to common ground */
+   
+/* Initialise with default values (int time = 2.4ms, gain = 1x) */
+// Adafruit_TCS34725 tcs = Adafruit_TCS34725();
+
+/* Initialise with specific int time and gain values */
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+neoPixel lightStick(8,4);
+
+void setup(void) {
+  //Serial.begin(9600);
+  
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1);
+  }
+  lightStick.begin();
+  
+  // Now we're ready to get readings!
+}
+
+void setStick(colorObj* color) {
+  
+  for(byte i=0;i<8;i++) {
+    lightStick.setPixelColor(i,color);
+  }
+  lightStick.show();
+}
+
+colorObj colorFromRaw(uint16_t r,uint16_t g, uint16_t b) {
+  
+  colorObj result;
+  result.setColor(r>>8,g>>8,b>>8);
+  return result;
+}
+  
+  
+void loop(void) {
+  uint16_t r, g, b, c, colorTemp, lux;
+  
+  tcs.getRawData(&r, &g, &b, &c);
+  colorTemp = tcs.calculateColorTemperature(r, g, b);
+  lux = tcs.calculateLux(r, g, b);
+  
+  Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
+  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
+  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
+  Serial.println(" ");
+  
+  colorObj result = colorFromRaw(r,g,b);
+  setStick(&result);
+}
