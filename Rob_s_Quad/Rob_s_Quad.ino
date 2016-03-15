84f421e895e7ab364a5d523bbe6f60eca4900515
Jim Lee <gitHub@leftcoast.biz>
2016-03-15 16:22:54 -0700

    Back to software
    Firing up Github again.

--- Rob_s_Quad/Rob_s_Quad.ino
+++ Rob_s_Quad/Rob_s_Quad.ino
@@ -11,7 +11,7 @@
 #include <chainPixels.h>
 #include "robbyRing.h"
 
-#define NUM_RING_LEDS   12
+#define NUM_RING_LEDS   4
 #define NUM_STICK_LEDS  8
 #define LED_PIN 3
 
@@ -19,16 +19,18 @@ chainPixels      theChain(LED_PIN);     // Create the chain to manage the LEDs.
 
 void setup() {
   
- 
+ /*
   robbyTailLight*  lightStick;
   lightStick = new robbyTailLight(NUM_STICK_LEDS);
   theChain.addGroup(lightStick);
+ */
  
   robbyRing* ring;                      // Pointer to a pixelGroup derived object.
   ring = new robbyRing(NUM_RING_LEDS);  // Create a new robbyRing object.
   theChain.addGroup(ring);              // Hand it off to the chainPixels object. 
                                         // If for some reason the chainPixels object
                                         // is told to let it go, it will delete it from memory.                                  
+  /*
   ring = new robbyRing(NUM_RING_LEDS);  
   theChain.addGroup(ring); 
 
@@ -37,7 +39,7 @@ void setup() {
 
   ring = new robbyRing(NUM_RING_LEDS);  
   theChain.addGroup(ring);  
-
+  */
   theChain.hookup();
 }
 
