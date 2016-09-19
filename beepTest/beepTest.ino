601ec9459ba6d56faf268c48a6c57aef092e043a
Jim Lee <github@leftcoast.biz>
2016-09-18 19:21:12 -0700

    number tweaks beepTest

--- beepTest/beepTest.ino
+++ beepTest/beepTest.ino
@@ -16,10 +16,10 @@
 #define WAVE_DURATION 1 // How long 'till the next wave. Pitch, kinda'
 #define WAVE_ON 0.1     // How long is the wave ON Smaller than duration.
 
-#define NOTE_TIME 20     // How long to hold a note.
+#define NOTE_TIME 200     // How long to hold a note.
 
 #define MIN_COUNT 0
-#define MAX_COUNT 9 // Number of notes 'till we repeat.
+#define MAX_COUNT 20 // Number of notes 'till we repeat.
 
 blinker beep(SOUND_PIN,WAVE_ON,WAVE_DURATION,true);
 timeObj timer(NOTE_TIME);
@@ -49,3 +49,4 @@ void loop() {
     if (count>MAX_COUNT) count = MIN_COUNT;
   }
 }
+
