480de7e154e23fbe2eb45fe8aa8ccfbb94a958cc
Jim Lee <gitHub@leftcoast.biz>
2016-09-26 17:37:54 -0700

    Updated Fly Bonnet.
    Wire match the "production" version and now it has "sparkles". :)

--- flyBonnet/flyBonnet.ino
+++ flyBonnet/flyBonnet.ino
@@ -16,23 +16,28 @@
 
 #include "shakeNTinkle.h"
 
-#define MIN_SHAKE     15      // Threshold vibration value for trigering reset of the sleep timer.
+#define MIN_SHAKE     10      // Threshold vibration value for trigering reset of the sleep timer.
+#define MORE_SHAKE    20      // Really shaking its head!
 #define MIN_TINKLE    20     // currently ignored
 #define SENS1_SWITCH_PIN 3
 #define SENS2_SWITCH_PIN 4
 #define SUM_TIME      40     // Ms to gather shakes and tinkles.
 #define NUM_DATA      5      // Data points per.
-#define FAST_DELAY    15     // How long to hold a white blink.
+#define FAST_DELAY    25     // How long to hold a white blink.
+#define DARK_PERCENT  75
 
 colorObj baseColor;
 blinker aBLinker;
-neoPixel lightBar(8,8);
-shakeNTinkle sensors(SENS1_SWITCH_PIN, SENS2_SWITCH_PIN,SUM_TIME,NUM_DATA);
+neoPixel lightBar(8, 8);
+shakeNTinkle sensors(SENS1_SWITCH_PIN, SENS2_SWITCH_PIN, SUM_TIME, NUM_DATA);
+long randomFlashNum1;
+long randomFlashNum2;
+timeObj fastTimer(FAST_DELAY);
 
 void setAll(colorObj* color) {
 
-  for(byte i=0;i<=7;i++) {
-    lightBar.setPixelColor(i,color);
+  for (byte i = 0; i <= 7; i++) {
+    lightBar.setPixelColor(i, color);
   }
   lightBar.show();   // Lest show it!
 }
@@ -41,7 +46,7 @@ void setup() {
 
   sensors.begin();
   baseColor.setColor(&blue);
-  colorObj temp = baseColor.blend(&black,90);
+  colorObj temp = baseColor.blend(&black, DARK_PERCENT);
   baseColor.setColor(&temp);
   lightBar.begin();
   aBLinker.hookup();
@@ -50,30 +55,59 @@ void setup() {
 
 }
 
+void flash(int inNum, colorObj* flashColor) {
+
+  //lightBar.setPixelColor(inNum,flashColor);
+  if (bitRead(randomFlashNum1, inNum) && bitRead(randomFlashNum1, inNum)) {
+    lightBar.setPixelColor(inNum, flashColor);
+  }
+}
+
+
 void shake1(void) {
 
-  lightBar.setPixelColor(3,&white);
-  lightBar.setPixelColor(4,&white);
+
+  flash(3, &white);
+  flash(4, &white);
   lightBar.show();   // Lest show it!
-  delay(FAST_DELAY);
-  lightBar.setPixelColor(2,&white);
-  lightBar.setPixelColor(5,&white);
-  lightBar.show();   // Lest show it!
-  delay(FAST_DELAY);
-  lightBar.setPixelColor(1,&white);
-  lightBar.setPixelColor(6,&white);
-  lightBar.show();   // Lest show it!
-  delay(FAST_DELAY);
-  lightBar.setPixelColor(0,&white);
-  lightBar.setPixelColor(7,&white);
-  lightBar.show();   // Lest show it!
-  delay(FAST_DELAY);
+  fastTimer.start();
+  while (!fastTimer.ding()) idle();
   setAll(&baseColor);
+
+  flash(2, &white);
+  flash(5, &white);
+  lightBar.show();   // Lest show it!
+  fastTimer.start();
+  while (!fastTimer.ding()) idle();
+  setAll(&baseColor);
+
+  flash(1, &white);
+  flash(6, &white);
+  lightBar.show();   // Lest show it!
+  fastTimer.start();
+  while (!fastTimer.ding()) idle();
+  setAll(&baseColor);
+
+  flash(0, &white);
+  flash(7, &white);
+  lightBar.show();   // Lest show it!
+  fastTimer.start();
+  while (!fastTimer.ding()) idle();
+
+  setAll(&baseColor);   // Show's over, reset the lights.
 }
 
 void loop() {
+
   idle();
   if (sensors.getShake() > MIN_SHAKE) {
+    randomFlashNum1 = random(0, 256);
+    randomFlashNum2 = random(0, 256);
+    shake1();
+  } 
+  if (sensors.getShake() > MORE_SHAKE) {
+    randomFlashNum1 = random(0, 256);
+    randomFlashNum2 = random(0, 256);
     shake1();
   }
 }
