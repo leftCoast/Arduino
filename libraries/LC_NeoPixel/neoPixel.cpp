55bfa51ea2e392b631159a4648f4f86853910873
Jim Lee <gitHub@leftcoast.biz>
2017-02-04 12:04:36 -0800

    First attempt on getting white pixels working
    Did it for a guy online. Have nothing to test it on so- We’ll see..

--- libraries/LC_NeoPixel/neoPixel.cpp
+++ libraries/LC_NeoPixel/neoPixel.cpp
@@ -12,6 +12,16 @@ void neoPixel::setPixelColor(uint16_t n,colorObj* inColor) {
     
     Adafruit_NeoPixel::setPixelColor(n,inColor->getRed(),inColor->getGreen(),inColor->getBlue());
  }
+ 
+ void neoPixel::setPixelColorW(uint16_t n,colorObj* inColor) { 
+    
+    byte r = inColor->getRed();
+    byte g = inColor->getGreen();
+    byte b = inColor->getBlue();
+    byte	minColor = min(r,g);
+    minColor = min(minColor,b);
+    Adafruit_NeoPixel::setPixelColor(n,r-minColor,g-minColor,b-minColor,minColor);
+ }
   
   
 colorObj neoPixel::getPixelColor(uint16_t n) {
@@ -26,9 +36,31 @@ colorObj neoPixel::getPixelColor(uint16_t n) {
 }
 
 
+colorObj neoPixel::getPixelColorW(uint16_t n) {
+   
+   uint32_t packColor;
+   uint8_t*    colorPtr;
+   
+   packColor = Adafruit_NeoPixel::getPixelColor(n);
+   colorPtr = (uint8_t*) &packColor;
+   colorObj result(colorPtr[2]+colorPtr[3],
+   								 colorPtr[1]+colorPtr[3],
+   								 colorPtr[0]+colorPtr[3]);
+   return result;
+}
+
+
 void neoPixel::setAll(colorObj* color) {
 
   for (int i = 0; i < numPixels(); i++) {
   	 setPixelColor(i, color);
+  }
+}
+
+
+void neoPixel::setAllW(colorObj* color) {
+
+  for (int i = 0; i < numPixels(); i++) {
+  	 setPixelColorW(i, color);
   }
 }
