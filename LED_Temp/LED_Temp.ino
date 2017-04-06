1c9ada817999e3819fba40101fe47a170782e8ad
Jim Lee <gitHub@leftcoast.biz>
2017-04-06 15:11:35 -0700

    Did the digital thermometer
    Whipped up a digital thermometer for the Coke machine. It seems like
    there's is a cooling issue. There is, didn't need a thermometer to see
    it. But its a cool little deal.

--- LED_Temp/LED_Temp.ino
+++ LED_Temp/LED_Temp.ino
@@ -1,17 +1,69 @@
 #include <idlers.h>
 #include <timeObj.h>
+#include <runningAvg.h>
+#include <LM35.h>
 
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_LEDBackpack.h>
 
-void setup() {
-  matrix.begin(0x70);              // pass in the address
-  matrix.setRotation(2);
+Adafruit_8x8matrix matrixL;// = Adafruit_8x8matrix();
+Adafruit_8x8matrix matrixR;
 
+timeObj tempTimer(1000);    // Gives us a reading every second.
+LM35 tempSensor(A0);        // Choose whatever analog pin you like.
+
+runningAvg  smoother(20);
+
+void setup() {
+  
+  matrixL.begin(0x70);              // pass in the address
+  matrixL.setRotation(2);
+  matrixL.setTextSize(1);
+  matrixL.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+  matrixL.setTextColor(LED_ON);
+  matrixL.setBrightness(15);
+
+  matrixR.begin(0x71);              // pass in the address
+  matrixR.setRotation(2);
+  matrixR.setTextSize(1);
+  matrixR.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+  matrixR.setTextColor(LED_ON);
+  matrixR.setBrightness(15);
+
+  tempTimer.start();
 }
 
-void loop() {
-  idle();
+void writeStr(char* inStr) {
   
+  matrixL.clear();
+  matrixL.setCursor(0,0);
+  matrixL.print(inStr);
+  
+  matrixR.clear();
+  matrixR.setCursor(-8,0);
+  matrixR.print(inStr);
+  
+  matrixL.writeDisplay();
+  matrixR.writeDisplay();
+}
+
+
+void loop() {
+  
+  char tempStr[10];
+  int tempF;
+  
+  idle();
+  if (tempTimer.ding()) {
+    tempF = round(smoother.addData(tempSensor.tempF()));
+    if (tempF<100) {
+      snprintf (tempStr,3,"%dd",tempF);
+      strcat(tempStr,"F");
+      writeStr(tempStr);
+    } else {
+      writeStr("+++");
+    }
+    tempTimer.stepTime();
+  }
 }
