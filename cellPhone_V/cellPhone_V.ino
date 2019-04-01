5a8662568db8f83e341da28d00a9fab17651b69b
Jim Lee <gitHub@leftcoast.biz>
2019-04-01 08:43:03 -0700

    Reset CellPhone_V to be test app.
    I need to get a handle on the damn SIM800 chip. I"m going to try making basically a simple terminal program.

--- cellPhone_V/cellPhone_V.ino
+++ cellPhone_V/cellPhone_V.ino
@@ -1,294 +1,82 @@
-  
-#include <Adafruit_GFX.h>
-#include <gfxfont.h>
-#include <Adafruit_ILI9341.h>
-#include <adafruit_1947_Obj.h>  // ADAFRUIT_1947
-#include <SD.h>
 
-#include <colorObj.h>
-#include <idlers.h>
-#include <lists.h>
-#include <mapper.h>
-#include <multiMap.h>
-#include <timeObj.h>
-#include <quickCom.h>
+//This one is all about simple test cases. I'm tired of being in the dark
+// on how this stupid thing works.
+ 
 
-#include <colorRect.h>
-#include <displayObj.h>
-#include <drawObj.h>
-#include <label.h>
-#include <lineObj.h>
-#include <screen.h>
+#include <SoftwareSerial.h>
+#include "Adafruit_FONA.h"
 
-#include <cellCommon.h>
+#define FONA_RX 9
+#define FONA_TX 8
+#define FONA_RST 4
+#define FONA_RI  7
 
-#include "litlOS.h"
-#include "editField.h"
-#include "textView.h"
-#include "keyboard.h"
-#include "IOandKeys.h"
-#include "icons.h"
+#define COM_BUFF_BYTES  255
 
 
-#define TFT_CS  10
-#define TFT_RST -1  // I think I remember this is not used.
-#define SD_CS   4
-
-#define TB_X    10
-#define TB_Y    20
-#define TB_W    220
-#define TB_H    148
-
-#define TF_INSET  3
-#define TF_X      TB_X+TF_INSET
-#define TF_Y      TB_Y+TF_INSET
-#define TF_W      TB_W-(2*TF_INSET)
-#define TF_H      TB_H-(2*TF_INSET)
-
-#define EB_X    TB_X
-#define EB_Y    TB_Y+TB_H+8
-#define EB_W    TB_W
-#define EB_H    16
-
-#define ET_INSET  3
-#define ET_X      EB_X+ET_INSET
-#define ET_Y      EB_Y+5
-#define ET_W      EB_W-(2*ET_INSET)
-#define ET_H      10
-
-#define BATT_X    199
-#define BATT_Y    3
-
-#define SIG_X     BATT_X + 15
-#define SIG_Y     BATT_Y + 1
-
-#define out  theTextView->appendText
-
-colorRect*    theTextBase;        // White rect behind the text indow.
-textView*     theTextView;        // The text window.
-colorRect*    theEditBase;        // White rect behind the text edit field.
-editField*    theEditField;       // The text edit field.
-IOandKeys*    ourKeyboard;        // Just like it sounds, our texting keyboard.
-battPercent*  battIcon;           // Icon thing showing charge state of the battery.
-RSSIicon*     sigIcon;            // Icon showing signal strength.
-closeBtn*     closeButton;        // Close the panel.
-
-qCMaster      ourComObj;          // Object used to comunicate with the FONA controller.
-
-timeObj       statusTimer(2500);  // We'll check status every 2 1/2 seconds. Well, 10 seconds for now.
-
-cellStatus    statusReg;
-
-enum comStates { offline, standby, comSent };
-comStates comState;
 
 
-enum  danceCards { noOne, statusBoy };
-danceCards  currCard;
+SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
+
+bool          FONAOnline;
+byte          comBuff[COM_BUFF_BYTES]; // Buffer for all comunication.
 
 
 void setup() {
-  
-  setupScreen();                        // Fire up the screen. If it fails it'll lock the process.
-  comState = offline;                   // Not online yet.
-  ourComObj.begin(9600);                // Fire up comunications.
-  if (ourComObj.readErr()==NO_ERR) {    // Did the poor thing fire up?
-    comState = standby;                 // Communications standing by!
-    out("FONA Coms, online.\n");
-  } else {
-    out("FONA Coms, FAIL.\n");
-  }
-  if (SD.begin(SD_CS)) {
-    out("File system ONLINE.\n");
+
+    Serial.begin(57600);
+    while (!Serial) {
+      
+    }
+    Serial.println("I'm here.");
     
-  } else {
-    out("File system FAIL.\n");
-  }
-  currCard = noOne;                     // Free to dance with any and all.
+    pinMode(0,INPUT);                               // Adafruit says to do this. Otherwise it may read noise.
+    pinMode(13, OUTPUT);                            // Our one and only debugging tool.
+    pinMode(FONA_RST, OUTPUT);            // Used for resetting the FONA.
+    resetFONA();                          // Hit reset, see if it'll come online.
+    Serial.print("FONA onine : ");Serial.println(FONAOnline);
 }
 
 
 void loop() {
+  uint16_t batteryVolts;
   
-  idle();
-
-  if (statusTimer.ding()&&comState==standby) {
-    if (currCard == noOne) {
-      //out("Giving card to status boy.\n");
-      currCard = statusBoy;
-      statusTimer.start();
-    }
+  //idle();
+  if (fona.getBattVoltage(&batteryVolts)) {             // Attempt to read the battery voltage..
+    Serial.print("Volts = ");Serial.println(batteryVolts);
+   } else {
+    Serial.print("Volts = ");Serial.println("Nothing but errors.");
   }
-  switch (currCard) {
-    case noOne      :                           break;
-    case statusBoy  : doStatus();               break;
-    //case SMSTalker  : ourKeyboard->sendSMS();   break;
-  }
-}
-
- 
-
-// **************************************************************
-// ************************** handlers **************************
-// **************************************************************
-
-
-void setupScreen(void) {
-
-  colorObj aColor(SYS_PANEL_COLOR);
-  
-  if (!initScreen(ADAFRUIT_1947,TFT_CS,TFT_RST,PORTRAIT)) {
-    while(true); // Kill the process.
-  }
-  
-  screen->fillScreen(&aColor);
-  
-  theTextBase   = new colorRect(TB_X,TB_Y,TB_W,TB_H,2);
-  theTextView   = new textView(TF_X,TF_Y,TF_W,TF_H);
-  theEditBase   = new colorRect(EB_X,EB_Y,EB_W,EB_H,2);
-  theEditField  = new editField(ET_X,ET_Y,ET_W,ET_H,"",1);
-  battIcon      = new battPercent(BATT_X,BATT_Y);
-  sigIcon       = new RSSIicon(SIG_X,SIG_Y);
-  closeButton   = new closeBtn;
-
-  closeButton->setCallback(closeClicked);
-  
-  viewList.addObj(theTextBase);
-  viewList.addObj(theTextView);
-  viewList.addObj(theEditBase);
-  viewList.addObj(theEditField);
-  viewList.addObj(battIcon);
-  viewList.addObj(sigIcon);
-  viewList.addObj(closeButton);
-  
-  ourKeyboard   = new IOandKeys(theEditField,theTextView);
-  theTextView->setTextColors(&black,&white);
-  battIcon->setPercent(0);
-  sigIcon->setRSSI(31);
-}
-
-void closeClicked(void) {
-
-  out("CLICKED!\n");
+  delay(1000);
 }
 
 
-void doStatus(void) {
+// *************************************************
+// ******************* handlers ********************
+// *************************************************
 
-  byte  ourCom;
-  byte  numBytes;
+void resetFONA(void) {
 
-  ourCom = getStatus;
-  switch(comState) {
-    case offline    : currCard = noOne; break;
-    case standby    : 
-      ourComObj.readErr();                          // Clear out old errors.
-      ourComObj.sendBuff(&ourCom,1,true);           // Attempt a send.
-      if (ourComObj.readErr()) {                    // Trouble?
-        comState = offline;                         // Its busted!
-        currCard = noOne;                           // Toss the card.
-      } else {
-        comState = comSent;                         // We set it.
-      }
-      break;
-    case comSent    :                               // The command has been sent, check for reply.
-      if (ourComObj.readErr()) {                    // Anything go wrong in there?
-        out("had error.\n");
-        comState = offline;                         // Its broken!
-        currCard = noOne;                           // toss out now usless dance card.
-      } else {
-        numBytes = ourComObj.haveBuff();            // If its there, it'll give back its size.
-        if (numBytes>0) {                           // We got byte count?
-          if (numBytes==sizeof(cellStatus)) {       // We got the right byte count?
-            ourComObj.readBuff((byte*)&statusReg);  // Stuff the data into the struct.
-            updateInfo();                           // Deal with having new data.
-          } else {                                  // Wrong number of bytes?
-            ourComObj.dumpBuff();                   // Who knows what it is, get rid of it.
-          }
-          comState = standby;                       // Well we're done with it.
-          currCard = noOne;                         // realease the machine.
-        }
-      }
-      break;
-    default : break;
-  }
-}
-
-     
-void updateInfo(void) {
-
-  battIcon->setPercent((byte)statusReg.batteryPercent);
-  sigIcon->setRSSI(statusReg.RSSI);
-  
-  /*
-  /out("Setting battery voltage : ");out(statusReg.batteryVolts);out("mV\n");
-  out("Setting battery percent : ");out(statusReg.batteryPercent);out("%\n");
-  out("RSSI                    : ");out(statusReg.RSSI);out("\n");
-  out("Net Stat : ");
-  switch (statusReg.networkStat) {
-    case 0 : out("Not registered"); break;
-    case 1 : out("Reg. (home)"); break;
-    case 2 : out("Reg. (searching)"); break;
-    case 3 : out("Denied"); break;
-    case 4 : out("Unknown"); break;
-    case 5 : out("Reg. (roaming)"); break;
-    default : out("Undefined"); break;
-  }
-  out("\n");
-  out("volume                  : ");out(statusReg.volume);out("\n");
-  out("callStat                : ");out(statusReg.callStat);out("\n");
-  out("numSMSs                 : ");out(statusReg.numSMSs);out("\n");
-  out("networkTime             : ");out(statusReg.networkTime);out("\n");
-  */
-
-}
-
-bool  sendComNBuff(byte aCom,byte* aBuff,byte numBytes,bool reply) {
-
-  byte* outBuff;
-
-  outBuff = (byte*)malloc(numBytes+1);              // Clear out some space.
-  if (outBuff) {                                    // If we got the space.
-    outBuff[0] = aCom;                              // Char 0 is the command.
-    for(byte i=0;i<numBytes;i++) {                  // Loop to fill in the buffer part.
-      outBuff[i+1] = aBuff[i];
-    }
-    ourComObj.readErr();                            // Clear out old errors.
-    ourComObj.sendBuff(outBuff,numBytes+1,reply);   // Attempt a send.
-    free(outBuff);                                  // Recycle the buffer.
-    return ourComObj.readErr()==NO_ERR;              // No error means we were able to send it out.
-  }
-  return false;                                     // If we got here something went wrong.
-}
-
-
-void  doSendSMS(void) {
+  FONAOnline = false;
+  digitalWrite(FONA_RST, HIGH);
+  delay(100);
+  digitalWrite(FONA_RST, LOW);
+  delay(10);
+  digitalWrite(FONA_RST, HIGH);
+  delay(100);
+  fonaSS.begin(4800);                   // For talking to the FONA.
+  FONAOnline = fona.begin(fonaSS);      // Able to fire up the FONA.
+  fona.setAudio(FONA_EXTAUDIO);         // Um.. Why is this here?
+  fona.setVolume(40);
+  fona.enableNetworkTimeSync(true);     // See if it works..
 
 /*
-  byte  ourCom;
-  byte  numBytes;
-
-  ourCom = sendSNS;
-  switch(comState) {
-    case offline    : currCard = noOne; break;
-    case standby    :
-      out("Me - ");                         // Reformat the output. Starting with, Me -
-      out(mOutBuff);                        // And the output sting.
-      if (sendComNBuff(sendSNS,ourKeyboard->mOutBuff,ourKeyboard->mNumBytes,true)) { // Send ok?
-        comState = comSent;                         // We set it.
-      } else {
-        comState = offline;                         // Its busted!
-        currCard = noOne;                           // Toss the card.
-      }
-    break;
-    case comSent    :                               // The command has been sent,
-  }
-  mComObj.sendBuff(&ourCom,1,true);           // Attempt a send.
-  if (!fona.sendSMS("14083400352", mOutBuff)) {
-    out(" - Failed\n"));
-  } else {
-    out(" ->\n");
-  }
-  currCard = noOne;
-  */
+For those who wonder : You can add a call fona
+setAudio(FONA_EXTAUDIO) to your initialization code or issue
+direct command 'AT+CHFA=1' towards SIM800 module.
+In my case this module is no longer losing SIM card when
+receiving or placing phone calls.
+Thanks for a tip!
+*/
 }
