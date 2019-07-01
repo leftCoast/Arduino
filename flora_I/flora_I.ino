7ac2958d06ba536a19b04d886714bc947934f5a3
Jim Lee <gitHub@leftcoast.biz>
2019-06-30 19:12:57 -0700

    Sensor working
    Had to swap wires around but now we have button & sensor.

--- flora_I/flora_I.ino
+++ flora_I/flora_I.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_DotStar.h>
+#include <Adafruit_seesaw.h>
 
 #include <blinker.h>
 #include <colorObj.h>
@@ -12,37 +13,51 @@
 #include <runningAvg.h>
 #include <timeObj.h>
 
-// There is only one pixel on the board
-#define NUMPIXELS 1 
 
-//Use these pin definitions for the ItsyBitsy M4
-#define DATAPIN    7
-#define CLOCKPIN   8
-
-Adafruit_DotStar px(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+// Pins for the Trinet MO dot-Star.
+#define D_STAR_DAT_PIN    7
+#define D_STAR_CLK_PIN   8
 
 
+Adafruit_DotStar px(1, D_STAR_DAT_PIN, D_STAR_CLK_PIN, DOTSTAR_BRG);
 
+Adafruit_seesaw ss;
+
+#define BUTTON_PIN  1
+#define MOTOR_1_PIN 3
+#define MOTOR_2_PIN 4
 void setup() {
 
-  px.begin(); // Initialize pins for output
-  px.show();  // Turn all LEDs off ASAP
+  px.begin();                             // Initialize the LED.
+  px.show();                              // Turn the LED off.
   
-  pinMode(0, OUTPUT); //
-  pinMode(1, OUTPUT); //
-  pinMode(2, INPUT_PULLUP);  // Motor button
-  pinMode(3, OUTPUT); // IN1 Motor
-  digitalWrite(3, LOW);
-  pinMode(4, OUTPUT); // IN2Motor
-  digitalWrite(4, LOW);
-  pinMode(13, OUTPUT); //
+  pinMode(BUTTON_PIN, INPUT_PULLUP);      // Motor button
+  pinMode(MOTOR_1_PIN, OUTPUT);           // IN1 Motor
+  digitalWrite(MOTOR_1_PIN, LOW);         // This direction off.
+  pinMode(MOTOR_2_PIN, OUTPUT);           // IN2Motor
+  digitalWrite(MOTOR_2_PIN, LOW);         // This direction off.
+  pinMode(13, OUTPUT);                    // Standard red LED.
   
-  Serial.begin(57600);
-
+  Serial.begin(57600);                    // Fire up serial port.
+  
+  if (!ss.begin(0x36)) {                  // Start up moisture sensor.
+    Serial.println("ERROR! no Sensor.");  // Failed!
+    while(1);                             // Lock here.
+  } else {
+    Serial.print("Sensor version: ");     // Sensor up.
+    Serial.println(ss.getVersion(), HEX); // Display verions number.
+  }
 }
+
 
 void loop() {
 
-  digitalWrite(13, !digitalRead(2));
-  digitalWrite(3, !digitalRead(2));
+  digitalWrite(13, !digitalRead(BUTTON_PIN));
+  digitalWrite(MOTOR_1_PIN, !digitalRead(BUTTON_PIN));
+
+  float tempC = ss.getTemp();
+  uint16_t capread = ss.touchRead(0);
+
+  Serial.print("Temperature: "); Serial.print(tempC); Serial.print("*C   ");
+  Serial.print("Capacitive: "); Serial.println(capread);
 }
