bcceb68b5e5eec0d60aa3ae3cb07f0a2eee399ad
Jim Lee <gitHub@leftcoast.biz>
2020-06-04 22:17:43 -0700

    Update servo_box.ino
    Wrote a quick program to test what I could of the wiring. Screen, Pot, button. Everyhting tested worked fine.

--- servo_box/servo_box.ino
+++ servo_box/servo_box.ino
@@ -1,3 +1,8 @@
+#include "screen.h"
+#include "adafruit_1431_Obj.h"
+#include "idlers.h"
+#include "colorRect.h"
+#include "runningAvg.h"
 
 #define OLED_CS   10
 #define SD_CS     4
@@ -6,12 +11,35 @@
 #define POT_PIN   A0
 #define SERVO_PIN 22
 
-void setup() {
-  // put your setup code here, to run once:
+mapper      percentMap(1023,0,0,100);
+runningAvg  smoother(20);
+colorMapper swatchMapper;
+colorRect   swatch(10,10,10,10);
 
+void setup() {
+   if (!initScreen(ADAFRUIT_1431, OLED_CS, OLED_RST, INV_PORTRAIT)) {
+      while (1);
+   }
+   screen->fillScreen(&green);
+   pinMode(POT_BTN, INPUT_PULLUP);
+   swatch.setColor(&blue);
+   viewList.addObj(&swatch);
+   swatchMapper.setColors(&green,&blue);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  int       rawPotVal;
+  float     percent;
+  colorObj  aColor;
+  
+  idle();         // Idlers get their time.
+  rawPotVal = analogRead(POT_PIN);
+  percent = percentMap.Map(rawPotVal);
+  percent = smoother.addData(percent);
+  aColor = swatchMapper.Map(percent);
+  swatch.setColor(&aColor);
+  if (!digitalRead(POT_BTN)) {
+      screen->fillScreen(&aColor);
+  }
 }
