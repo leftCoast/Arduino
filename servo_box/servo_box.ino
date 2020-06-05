25e3c67312ea4a4910cf5c1270706ca6c780e931
Jim Lee <gitHub@leftcoast.biz>
2020-06-04 18:30:15 -0700

    Create servo_box.ino

---
+++ servo_box/servo_box.ino
@@ -0,0 +1,17 @@
+
+#define OLED_CS   10
+#define SD_CS     4
+#define OLED_RST  6
+#define POT_BTN   2
+#define POT_PIN   A0
+#define SERVO_PIN 22
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
