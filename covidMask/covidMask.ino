3d028e30eb5fd5a7532196e6110d8baa948c112a
Jim Lee <gitHub@leftcoast.biz>
2020-08-19 17:35:18 -0700

    Create covidMask.ino

---
+++ covidMask/covidMask.ino
@@ -0,0 +1,53 @@
+#include <chainPixels.h>
+#include <neoPixel.h>
+
+#include <blinker.h>
+#include <colorObj.h>
+#include <debug.h>
+#include <idlers.h>
+#include <lists.h>
+#include <mapper.h>
+#include <mechButton.h>
+#include <multiMap.h>
+#include <resizeBuff.h>
+#include <runningAvg.h>
+#include <squareWave.h>
+#include <textBuff.h>
+#include <timeObj.h>
+
+neoPixel      bioRing(12,2);
+colorMultiMap bioColorMapper;
+runningAvg    smoother(20);
+
+void setup() {
+
+  Serial.begin(9600);
+  bioRing.begin();
+  bioRing.setAll(&black);
+  bioRing.show();
+  /*
+  while(true) {
+  bioRing.setAll(&blue);
+  bioRing.show();
+  delay(500);
+  bioRing.setAll(&red);
+  bioRing.show();
+  delay(500);
+  }
+  */
+  bioColorMapper.addColor(960,&red);
+  bioColorMapper.addColor(948,&blue);
+}
+
+void loop() {
+  
+  colorObj  bioColor;
+  int       vacVal;
+  
+  vacVal = analogRead(A5);
+  vacVal = smoother.addData(vacVal);
+  Serial.println(vacVal);
+  bioColor = bioColorMapper.Map(vacVal);
+  bioRing.setAll(&bioColor);
+  bioRing.show();
+}
