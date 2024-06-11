7ba354efd8c24134d639c3b19bc9611ed42a29e8
Jim Lee <gitHub@leftcoast.biz>
2024-06-10 23:32:48 -0700

    Got MIDI import kinda' working

--- libraries/LC_piezoTunes/toneObj.cpp
+++ libraries/LC_piezoTunes/toneObj.cpp
@@ -1,5 +1,9 @@
-#include "toneObj.h"
-#include "Arduino.h"
+#include <toneObj.h>
+#include <Arduino.h>
+#include <MIDINotes.h>
+#include <strTools.h>
+#include <SDTools.h>
+
 
 //#include "debug.h"
 
@@ -128,42 +132,99 @@ void tune::idle(void) {
 }
 
       	      
-//
-//
-//
-/*
-WriteVarLen(long value) {
+// ***************************************************************************************
+// Basically the same as a tune but.. Build from a MIDI file. (Single voice)
+// ***************************************************************************************
 
-	long buffer;
+
+MIDItune::MIDItune(const char* inFilePath)
+	: tune() {
 	
-	buffer = value & 0x7f;
-	while((value >>= 7) > 0) {
-		buffer <<= 8;
-		buffer |= 0x80;
-		buffer += (value & 0x7f);
+	filePath = NULL;
+	heapStr(&filePath,inFilePath);
+}
+
+
+MIDItune::~MIDItune(void) { MIDIFile.close(); }
+
+void MIDItune::createTune(void) {
+
+	BYTE_SWAP
+	
+   File        MIDIFile;
+   MIDIHeader  theMIDIHeader;
+   trackHeader theTrackHeader;
+   eventHeader anEventHeader;
+   metaEvent   aMetaEvent;
+   byte        aByte;
+   uint32_t		temp;
+   uint32_t    microsPerBeat;
+   float			MsPerTick;
+   bool        done;
+   int			DnNote;
+   uint32_t		StTime;
+   uint32_t		EndTime;
+   
+   DnNote	= 0;	
+   StTime	= 0;
+   EndTime	= 0;			
+	MIDIFile = SD.open(filePath, FILE_READ);
+	if (MIDIFile) {
+		
+		readMIDIHeader(&theMIDIHeader, MIDIFile);
+		readTrackHeader(&theTrackHeader, MIDIFile);
+		done = false;
+		while(!done) {
+			readEventHeader(&anEventHeader, MIDIFile);
+			if (isMetaTag(&anEventHeader)) {
+            readMetaEvent(&anEventHeader, &aMetaEvent, MIDIFile);
+            switch(aMetaEvent.metaType) {
+               case 0x2F : done = true; break;
+               case 0x51 :
+						microsPerBeat = 0;
+                  MIDIFile.read(&aByte, 1);
+                  microsPerBeat = aByte;
+                  microsPerBeat = microsPerBeat << 16;
+                  MIDIFile.read(&aByte, 1);
+                  temp = 0;
+                  temp = aByte;
+                  temp = temp << 8;
+                  microsPerBeat = microsPerBeat | temp;
+                  MIDIFile.read(&aByte, 1);
+                  temp = 0;
+                  temp = aByte;
+                  microsPerBeat = microsPerBeat | aByte;
+                  MsPerTick = (microsPerBeat/1000.0)/theMIDIHeader.timeDiv;
+               break;
+               default :
+                  done = !jumpMetaEvent(&aMetaEvent, MIDIFile);
+               break;
+            }
+         } else {
+         	switch(anEventHeader.eventType) {
+         		case MIDI_KEY_DN : 
+         			if (DnNote) {
+         				EndTime = StTime + anEventHeader.deltaTime;
+         				addNote(MIDI2Freq(DnNote),anEventHeader.deltaTime);
+         				DnNote = anEventHeader.param1;
+         				StTime = EndTime;
+         			} else {
+         				EndTime = StTime + anEventHeader.deltaTime;
+         				addNote(REST,anEventHeader.deltaTime);
+         				DnNote = anEventHeader.param1;
+         				StTime = EndTime;
+         			}
+         		break;	
+         		case MIDI_KEY_UP :
+         			if (DnNote) {
+         				addNote(MIDI2Freq(DnNote),anEventHeader.deltaTime);
+         				EndTime = StTime + anEventHeader.deltaTime;
+         			}
+         		break;
+         	}		
+         }
+		}
+		MIDIFile.close();
 	}
-	while (1) {
- 		putc(buffer,outfile);
- 		if(buffer & 0x80) {
- 			buffer >>= 8;
- 		} else
- 			break;
- 		}
- 	}
 }
-
-
-unsigned long ReadVarLen(void) {
-
-	unsigned long	value;
-	byte				c;
-
-	if((value = getc(infile)) & 0x80) {
- 		value &= 0x7f;
- 		do {
-  			value = (value << 7) + ((c = getc(infile))) & 0x7f);
-  		} while (c & 0x80);
- 	}
-	return(value);
-}
-*/
+           
