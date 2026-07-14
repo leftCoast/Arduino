fbccb00b0a8797d817128061bdeadff44233022b
Jim Lee <github@leftcoast.biz>
2026-07-13 23:48:46 -0700

    Pulled out LC_icons

--- libraries/LC_icons/LC_icons.h
+++
@@ -1,9 +0,0 @@
-
-//  LC_icons is currently where all the icons I've been creating are being stored.
-//  Most likely in the future this will be changed to a better system/location etc.
-//  But for now, at least they are in some sort of source control.
-//
-//  This .h file is to shut up the damn Ardunio IDE because it has fits when it sees
-//  a library folder with no source files in it.
-//
-//   So don't go including this in any source files. It won't help anything.
